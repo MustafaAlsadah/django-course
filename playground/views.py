c163578f669dbec3048caf9ecb5359b2998857c3
MustafaAlsadah <sadah9999@gmail.com>
2022-04-19 16:13:14 +0300

    querying generic types models

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,24 @@
-from itertools import product
+from __future__ import annotations
+from ast import Or
+from itertools import count, product
+from multiprocessing.sharedctypes import Value
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F
-from store.models import Product, Customer, Collection, Order, OrderItem
+from django.db.models import Q, F, Value, FloatField, Func, ExpressionWrapper, DecimalField
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from store.models import Product, Customer, Collection, Order, OrderItem, Collection
+from tags.models import TaggedItem
 
 def say_hello(request):
-    product_queryset = Product.objects.filter(
-        Q(orderitem__product_id=F('id'))
-        ).distinct().order_by('title')
+    content_type = ContentType.objects.get_for_model(Product)
+    queryset = TaggedItem.objects.select_related('tag').filter(
+        content_type=content_type,
+        object_id=1
+    )
 
     return render(request, 'hello.html', {
         'name': 'Mustafa',
-        'products':list(product_queryset)
+        'orders': queryset
     })

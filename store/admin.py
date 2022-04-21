dac54072a8250cf98a1033e7ac5aa1983ce9d313
MustafaAlsadah <sadah9999@gmail.com>
2022-04-22 00:00:19 +0300

    a

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
